7c14220b4135ee150d533ab2e86cdb109d234ce3
Evandro <capieper.pieper@gmail.com>
2019-11-22 23:31:25 -0300

    o merda

--- emagrecer/src/views/component/posts.js
+++ emagrecer/src/views/component/posts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { getId } from '../../services/auth';
 
 class Post extends Component {
+  authors="";  
   constructor(props) {
     super(props);
     this.state = {
@@ -22,13 +23,14 @@ class Post extends Component {
     var self = this;
     let queryString = new URLSearchParams();
     queryString.append("id", getId());
-    fetch('https://localhost:5001/Forum/ListForums?' + queryString, { headers: { 'Content-Type': 'application/json' } })
+        fetch("https://localhost:5001/Forum/ListUserForums?" + queryString, { headers: { 'Content-Type': 'application/json' } })
       .then(function (response) {
         response.json().then(function (data) {
-          self.setState({ posts: data.forums });
-          console.log(self.state.posts);
+          self.setState({ Mp: data.author });
+          console.log(self.Mp);
         });
       });
+      
   }
 
   render() {
@@ -41,11 +43,11 @@ class Post extends Component {
           <div className="card small forum">
             <a className="text" href="post.html">
               <div className="card-image">
-                <img src={self.state.posts.picture} />
-                <span className="card-title">{self.state.posts.title}</span>
+                <img  />
+                <span className="card-title"></span>
               </div>
               <div className="card-content">
-                <p >{self.state.posts.text}</p>
+                <p ></p>
               </div>
             </a>
           </div>
